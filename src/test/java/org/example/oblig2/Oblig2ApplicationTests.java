5ab1e2de7d0679ec2034ad0daf577b66dbb7d830
Suukhii <sukhmanikakar11@gmail.com>
2024-04-18 14:48:00 +0200

    created springprojct

---
+++ src/test/java/org/example/oblig2/Oblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
